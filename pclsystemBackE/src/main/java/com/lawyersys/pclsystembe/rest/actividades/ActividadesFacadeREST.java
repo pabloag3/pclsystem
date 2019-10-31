c0036bf2e89b12aa8cbbeae445c7461c486a1c50
PabloAg <tatoaguilar95@gmail.com>
2019-10-31 16:53:22 -0300

    Servicio para traer actividades pendientes-caducadas
    Se crea un servicio para traer las actividades pendientes y caducadas de un empleado dado.

--- pclsystemBackE/src/main/java/com/lawyersys/pclsystembe/rest/actividades/ActividadesFacadeREST.java
+++ pclsystemBackE/src/main/java/com/lawyersys/pclsystembe/rest/actividades/ActividadesFacadeREST.java
@@ -89,6 +89,19 @@ public class ActividadesFacadeREST {
             return ErrorManager.manejarError(e, Actividades.class);
         }
     }
+    
+    @GET
+    @Path("traer-pendientes-caducado-del-empleado/{id}")
+    public Response traerPendienteCaducadoDelEmpleado (@PathParam("id") String id) throws JsonProcessingException {
+        try {
+            List<Actividades> elem = (List<Actividades>) (Object) abmManager.traerPendienteCaducadoDelEmpleado(id);
+            ObjectMapper mapper = new ObjectMapper();
+            String resp = mapper.writeValueAsString(elem);
+            return Response.ok(resp).build();
+        } catch (Exception e) {
+            return ErrorManager.manejarError(e, Actividades.class);
+        }
+    }
 
     @GET
     @Path("listar")
@@ -101,7 +114,6 @@ public class ActividadesFacadeREST {
         } catch (Exception e) {
             return ErrorManager.manejarError(e, Actividades.class);
         }
-        
     }
     
 }
